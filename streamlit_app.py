6fe2408e5faa5333d53959c59bf2c69dd4795dfc
matspagels <104843279+matspagels@users.noreply.github.com>
2022-05-10 23:49:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ def main():
 	st.title("Welcome to Nova´s own private Horseracing Track!")
 
 	menu = ["Welcome", "Tutorial", "Login", "SignUp"]
-	choice = st.sidebar.selectbox("Menu",menu)
+	choice = st.sidebar.button("Menu",menu)
 
 	if choice == "Welcome":
 		st.subheader("Glad to have you here!")
