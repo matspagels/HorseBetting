be791340f60b7b7861741907a8ae11d89cb5cff8
matspagels <104843279+matspagels@users.noreply.github.com>
2022-05-10 23:20:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import math
 import pandas as pd
 import streamlit as st
 
-subheader("Welcome to BetCoin™!")
+print(subheader("Welcome to BetCoin™!"))
 
-        write("Create a user or login with your existing account.")
-        write("Every new user starts with a blance of 100.")
-        write("You can bet on 5 different crypto coins to go up or down within the next 15 seconds.")
-        write("Bets entries range between 1 and 10. The return is double or nothing.")
-        write("Look on the ranking page how you compare to other players.")
-        write("Have fun!")
+        print("Create a user or login with your existing account.")
+        print("Every new user starts with a blance of 100.")
+        print("You can bet on 5 different crypto coins to go up or down within the next 15 seconds.")
+        print("Bets entries range between 1 and 10. The return is double or nothing.")
+        print("Look on the ranking page how you compare to other players.")
+        print("Have fun!")
 
-        subheader("Login or Create a User")
+        print(subheader("Login or Create a User"))
